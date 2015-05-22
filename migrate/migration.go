8c7149e6b3c699031fae53ca18807d0fe3ec0297
funnythingz <funnythingz69+github@gmail.com>
2015-05-22 15:41:21 +0900

    Fixed migration

--- migrate/migration.go
+++ migrate/migration.go
@@ -8,7 +8,34 @@ import (
 
 func main() {
 	db.Connect()
-	migrate()
+
+	action := "migrate"
+	if len(os.Args) >= 2 {
+		env = os.Args[1]
+	}
+
+	log.Println(fmt.Sprintf("mode: %s", action))
+
+	switch {
+	case action == "migrate":
+		Migrate()
+		return
+
+	case action == "reset":
+		Reset()
+		return
+	}
+}
+
+func Reset() {
+	log.Println(db.Dbmap.DropTableIfExists(&mapper.Iro{}))
+	log.Println(db.Dbmap.DropTableIfExists(&mapper.Color{}))
+	Create()
+}
+
+func Create() {
+	log.Println(db.Dbmap.CreateTable(&mapper.Iro{}))
+	log.Println(db.Dbmap.CreateTable(&mapper.Color{}))
 }
 
 func migrate() {
