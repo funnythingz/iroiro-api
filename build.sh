9e1df3a1fc6457fee6049386249df1a4a6a67f44
funnythingz <funnythingz69+github@gmail.com>
2015-05-25 23:18:26 +0900

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 GOOS=linux gom build -o iroiro
-tar cvfz iroiro.tar.gz ./iroiro ./database.toml
+tar cvfz iroiro.tar.gz ./iroiro ./db/database.toml
 mv ./iroiro.tar.gz ~/vagrant/iroiro/
 rm ./iroiro
 echo done
