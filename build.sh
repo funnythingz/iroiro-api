e30beceac2928f0c4efbe150705d7acae11cac71
funnythingz <funnythingz69+github@gmail.com>
2015-05-25 23:28:32 +0900

    added migration build

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
+# iroiro-api
 GOOS=linux gom build -o iroiro
 tar cvfz iroiro.tar.gz ./iroiro ./db/database.toml
 mv ./iroiro.tar.gz ~/vagrant/iroiro/
 rm ./iroiro
 echo done
+
+# migration
+GOOS=linux gom build migrate/migration.go
+tar cvfz migration.tar.gz ./migration ./db/database.toml
+mv ./migration.tar.gz ~/vagrant/iroiro/
+rm ./migration
+echo migration done
