fa1facb03512b81ba6d230489ef653ad418d8991
funnythingz <funnythingz69+github@gmail.com>
2015-06-20 19:44:51 +0900

    Fixed deploy script

--- ops/docker-deploy.sh
+++ ops/docker-deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# API
 APP=iroiro-api
 docker rm -f $APP
 docker pull tutum.co/funnythingz/$APP
-docker run -dit --name iroiro -p 80:8000 tutum.co/funnythingz/$APP /iroiro production
+docker run -dit --name $APP -p 80:8000 tutum.co/funnythingz/$APP /iroiro production
 
-# migration
-docker rm -f iroiro-migration
-docker pull tutum.co/funnythingz/iroiro-migration
-docker run -dit --name iroiro-migration tutum.co/funnythingz/iroiro-migration /migration production migrate
+APP=iroiro-api-migration
+docker rm -f $APP
+docker pull tutum.co/funnythingz/$APP
+docker run -dit --name $APP tutum.co/funnythingz/$APP /$APP production migrate
